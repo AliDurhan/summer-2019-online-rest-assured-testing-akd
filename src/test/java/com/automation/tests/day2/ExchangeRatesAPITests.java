589a87d32a4356e7998df08f559751c1e6018f67
AliDurhan <alikemaldurhan@gmail.com>
2020-06-01 21:47:13 -0500

    exchange Rates 3 tests added

--- src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
+++ src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
@@ -1,5 +1,6 @@
 package com.automation.tests.day2;
 
+import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
@@ -11,5 +12,40 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ExchangeRatesAPITests {
 
     private String baseURI = "https://api.openrates.io/";
+
+
+    @Test
+    public void test1() {
+        Response response = given().
+                get(baseURI + "latest");
+        //verify status code
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.prettyPrint());
+    }
+
+    @Test
+    public void test2() {
+        Response response = given().get(baseURI + "latest");
+        //verify that content type is json
+        assertEquals(200, response.getStatusCode());
+        //verify that data is coming as json
+        assertEquals("application/json", response.getHeader("Content-Type"));
+        //or like this
+        assertEquals("application/json", response.getContentType());
+    }
+
+    //GET https://api.exchangeratesapi.io/latest?base=USD HTTP/1.1
+    //base it's a query parameter that will ask web service to change currency from eu to something else
+    @Test
+    public void test3() {
+        //#TASK: get currency exchange rate for dollar. By default it's euro.
+//        Response response = given().get(baseURI+"latest?base=USD");
+        Response response = given().
+                baseUri(baseURI + "latest").
+                queryParam("base", "USD").
+                get();
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.prettyPrint());
+    }
 
 }
