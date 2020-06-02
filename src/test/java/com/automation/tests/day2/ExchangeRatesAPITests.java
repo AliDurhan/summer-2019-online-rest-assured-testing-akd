f7abf767e1d69b6086db7f01a2392bcbb90f8f1e
AliDurhan <alikemaldurhan@gmail.com>
2020-06-01 22:03:43 -0500

    pulling the rates from history

--- src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
+++ src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
@@ -41,11 +41,43 @@ public class ExchangeRatesAPITests {
         //#TASK: get currency exchange rate for dollar. By default it's euro.
 //        Response response = given().get(baseURI+"latest?base=USD");
         Response response = given().
-                baseUri(baseURI + "latest").
-                queryParam("base", "USD").
-                get();
+                            baseUri(baseURI + "latest").
+                            queryParam("base", "USD").
+                            get();
         assertEquals(200, response.getStatusCode());
         System.out.println(response.prettyPrint());
     }
+
+    //    #TASK: verify that response body, for latest currency rates, contains today's date (2020-01-23 | yyyy-MM-dd)
+    @Test
+    public void test4() {
+        Response response = given().
+                baseUri(baseURI + "latest").
+                queryParam("base", "GBP").
+                get();
+
+        String todaysDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        System.out.println("Today's date: " + todaysDate);
+
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.getBody().asString().contains(todaysDate));
+    }
+
+    //let's get currency exchange rate for year 2000
+    //GET https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&base=USD&symbols=ILS,JPY
+
+    @Test
+    public void test5() {
+        Response response = given().
+                            baseUri(baseURI + "history").
+                            queryParam("start_at", "2000-01-01").
+                            queryParam("end_at", "2000-12-31").
+                            queryParam("base", "USD").
+                            queryParam("symbols", "EUR", "GBP", "JPY").
+                            get();
+        System.out.println(response.prettyPrint());
+    }
+
 
 }
